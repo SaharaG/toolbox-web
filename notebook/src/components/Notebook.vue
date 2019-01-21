601c46529c9bef241b265b3e1058b3070d14b99a
夏飞 <xf188866@alibaba-inc.com>
2019-01-21 17:12:00 +0800

    notebook提交

---
+++ notebook/src/components/Notebook.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="font">
+  <textarea id="edit"></textarea>
+  </div>
+</template>
+
+<script>
+  import 'iview/dist/styles/fonts/ionicons.svg'
+  import 'iview/dist/styles/fonts/ionicons.ttf'
+  import 'iview/dist/styles/fonts/ionicons.woff'
+  import 'codemirror/lib/codemirror.css'
+  import 'codemirror/addon/display/fullscreen.css'
+  import 'codemirror/theme/night.css'
+  import codeMirror from 'codemirror'
+  import 'codemirror/addon/display/fullscreen'
+
+
+
+  export default {
+    name: "Notebook",
+    mounted: function () {
+      let edit = codeMirror.fromTextArea(document.getElementById('edit'), {
+        lineNumbers: true,
+        "fullScreen": true
+      });
+      console.log(edit);
+    }
+  }
+
+</script>
+
+<style scoped>
+  #font{
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  }
+
+</style>
