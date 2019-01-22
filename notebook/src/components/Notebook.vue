da791e54fdcb62f09b46466c1450642c1de083b1
夏飞 <xf188866@alibaba-inc.com>
2019-01-22 12:28:04 +0800

    成功

--- notebook/src/components/Notebook.vue
+++ notebook/src/components/Notebook.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="font">
-  <textarea id="edit"></textarea>
+    <textarea id="edit"></textarea>
   </div>
 </template>
 
@@ -11,25 +11,37 @@
   import 'codemirror/theme/night.css'
   import codeMirror from 'codemirror'
   import 'codemirror/addon/display/fullscreen'
-
+  import axios from 'axios'
 
 
   export default {
     name: "Notebook",
+
     mounted: function () {
       let edit = codeMirror.fromTextArea(document.getElementById('edit'), {
         lineNumbers: true,
         "fullScreen": true
       });
-      console.log(edit);
+      let instance = axios.create({
+        baseURL: 'https://api.xia-fei.com',
+        timeout: 1000,
+      });
+      instance.get('/map?key=note').then(res => {
+        let text = res.data;
+        edit.setValue(text + '');
+      });
+      edit.on('change', function (event) {
+        instance.post('/map?key=note&value=' + edit.getValue());
+      });
+
     }
   }
 
 </script>
 
 <style scoped>
-  #font{
-    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  #font {
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   }
 
 </style>
